d2bebda7bb73c94acff02c15266b39c22cbd7ace
r.a <2023-1-00960@lpu.edu.ph>
2025-04-05 10:36:47 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBXDrDeDnGHuq384a9BEuCoWiBvODux094",
+  authDomain: "crud-6c4cb.firebaseapp.com",
+  databaseURL: "https://crud-6c4cb-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-6c4cb",
+  storageBucket: "crud-6c4cb.firebasestorage.app",
+  messagingSenderId: "555079606813",
+  appId: "1:555079606813:web:7fe83dbdf30fecc5875cf4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
